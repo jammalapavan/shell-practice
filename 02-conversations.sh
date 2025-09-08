7dccd97cebf81468e52327258961154502edf990
jammalapavan <pavanjammala22@example.com>
2025-09-08 17:03:42 +0530

    shell-practice

---
+++ 02-conversations.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+echo "Trump: hey musk how are you"
+echo "musk:hey trump i am good how are you doing"
+echo "trump:i am fine. whats up"
+echo "musk:nothing. just going to mars. will come with me "
+echo "trump: sorry yo carry on.once you will come back! i will join you"
