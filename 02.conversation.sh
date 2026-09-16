0d8d0147474dfdddfd616d0472ee7f19564410b8
jammalapavan <pavanjammala22@example.com>
2026-09-16 11:07:57 +0530

    shell

---
+++ 02.conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+
+echo "trump:: hey musk, how are you"
+echo "musk::  hello trump, i am fine how are you doing"
+echo "trump:: i am fine to whats up"
+echo "musk:: nothing just going to the mars will you come"
+echo "trump:: thanks musk, you can carry on"
