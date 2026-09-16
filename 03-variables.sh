55887e941f3f5f550da2a629acb8911b9f29d608
jammalapavan <pavanjammala22@example.com>
2026-09-16 11:13:52 +0530

    shell

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+
+PERSON1=Trump
+PERSON2=musk
+
+echo "$PERSON1:: hey $PERSON2, how are you"
+echo "$PERSON2::  hello $PERSON1, i am fine how are you doing"
+echo "$PERSON1:: i am fine to whats up"
+echo "$PERSON2:: nothing just going to the mars will you come"
+echo "$PERSON1:: thanks $PERSON2, you can carry on"
